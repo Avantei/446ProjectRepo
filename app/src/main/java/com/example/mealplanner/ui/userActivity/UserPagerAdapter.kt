f47dc300107f55ce1b55fc650dd0461fb61aa2b8
Yunt Flare <peter.liang@uwaterloo.ca>
2023-06-26 23:24:07 -0400

    update viewPager to ViewPager2

--- app/src/main/java/com/example/mealplanner/ui/userActivity/UserPagerAdapter.kt
+++ app/src/main/java/com/example/mealplanner/ui/userActivity/UserPagerAdapter.kt
@@ -9,15 +9,14 @@ import com.example.mealplanner.R
 import com.google.android.material.tabs.TabLayout
 import com.google.android.material.tabs.TabLayoutMediator
 
+private val TAB_TITLES = listOf(
+    R.string.groups_tab_name,
+    R.string.profile_tab_name
+)
+
 class UserPagerAdapter(private val context: Context, fa: FragmentActivity) :
     FragmentStateAdapter(fa) {
-    private val _tabs = listOf(
-        R.string.groups_tab_name,
-        R.string.profile_tab_name
-    )
-
-    override fun getItemCount() = _tabs.size
-
+    override fun getItemCount() = TAB_TITLES.size
     override fun createFragment(position: Int): Fragment {
         return when (position) {
             0 -> GroupsFragment()
@@ -25,10 +24,9 @@ class UserPagerAdapter(private val context: Context, fa: FragmentActivity) :
             else -> ProfileFragment()
         }
     }
-
     fun attachMediator(tabs: TabLayout, viewPager: ViewPager2) {
         TabLayoutMediator(tabs, viewPager) { tab, position ->
-            tab.text = context.resources.getString(_tabs[position])
+            tab.text = context.resources.getString(TAB_TITLES[position])
         }.attach()
     }
 }
