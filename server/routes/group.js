d1cfa4ee40e1302729bb2bef517305cf48686dcf
Yunt Flare <peter.liang@uwaterloo.ca>
2023-07-04 03:15:02 -0400

    update group to use new middleware

--- server/routes/group.js
+++ server/routes/group.js
@@ -1,30 +1,45 @@
 const express = require("express");
 const GroupDal = require("../dal/GroupDal");
-const jwt = require("../middleware/auth");
+const { sessionValidations } = require("../middleware/auth");
+const validation = require("../middleware/InputValidation");
 const router = express.Router();
 
 const groupDal = new GroupDal();
 
-// Create a group
-router.post("/create", jwt.authMiddleware, async (req, res, next) => {
-  const userId = req.jwtData.userId;
-  const groupName = req.body.name;
-
-  const { data, err } = await groupDal.createGroup(groupName, userId);
-  if (err) next(err);
-  if (!data) next("Failed to create group"); // should not happen unless db error
-  return res.status(201).send(data);
-});
-// Get details of a group (name, memberIds, eventIds)
-router.get("/:groupId", jwt.authMiddleware, async (req, res, next) => {
-  const userId = req.jwtData.userId;
-  const groupId = req.params.groupId;
-  // check if user is in the group
-  const { data, err } = await groupDal.findGroupWithMember(groupId, userId)
-  if (err) next(err)
-  if (!data) return res.status(400).send("Bad Request: no matching group found");
-  return res.status(200).send(data);
-});
+/**
+ * Create a group
+ * Expect: validate jwt token, existing user
+ * Request: {name: string}
+ * Response: error or the group created
+ */
+router.post("/create", ...sessionValidations, validation.groupCreation,
+  async (req, res, next) => {
+    const userId = req.jwtData.userId;
+    const groupName = req.body.name;
+    const { data, err } = await groupDal.createGroup(groupName, userId);
+    if (err) return next(err);
+    if (!data) return next("Failed to create group"); // should not happen unless db error
+    return res.status(201).send(data);
+  }
+);
+/**
+ * get detail of a group
+ * Expect: validate jwt token, existing user
+ * Expect: user must be a member of the request group
+ * Request: N/A
+ * Response: error or the group requested
+ */
+router.get("/:groupId", ...sessionValidations,
+  async (req, res, next) => {
+    const userId = req.jwtData.userId;
+    const groupId = req.params.groupId;
+    // check if user is in the group
+    const { data, err } = await groupDal.findGroupWithMember(groupId, userId)
+    if (err) return next(err)
+    if (!data) return res.status(404).send("Not Found: no matching group found");
+    return res.status(200).send(data);
+  }
+);
 // Join a group, provide userId and groupId
 router.get("/join", async (req, res) => {
   res.status(404).send("Not Implemented")
